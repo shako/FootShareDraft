e69901639dd41d6f4b20f98ddcfcd696bd0d5dc1
Steven Machtelinckx <machtelinckx.consultancy@gmail.com>
2024-03-15 22:22:48 +0100

    Initial Commit

---
+++ FootShare/FootShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootShareApp.swift
+//  FootShare
+//
+//  Created by Steven Machtelinckx on 15/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct FootShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
