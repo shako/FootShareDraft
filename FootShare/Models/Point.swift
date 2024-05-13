1bff1b398f313d6a4148f943cc7e0bbf5cdbd6ce
Steven Machtelinckx <machtelinckx.consultancy@gmail.com>
2024-05-13 20:12:05 +0200

    remove debug comment

--- FootShare/Models/Point.swift
+++ FootShare/Models/Point.swift
@@ -28,7 +28,7 @@ extension Point {
     
     func madeDuring(_ session: Session) -> Bool {
         var madeDuringSession =  self.date >= session.startTime && (session.endTime != nil ? self.date <= session.endTime! : true)
-        debugPrint("point with date \(date.formatted()) made during session with startdate \(session.startTime.formatted()) and enddate \(session.endTime?.formatted() ?? "NONE")? -> \(madeDuringSession)")
+//        debugPrint("point with date \(date.formatted()) made during session with startdate \(session.startTime.formatted()) and enddate \(session.endTime?.formatted() ?? "NONE")? -> \(madeDuringSession)")
         return madeDuringSession
     }
     
